8efe83df68688dad1356935e0451606059274405
Thomas <thomasjameswright@hotmail.co.uk>
2020-04-07 22:52:09 +0100

    added help screen

--- Augmenting a Textbook/Assets/Scripts/MainMenuButtonManager.cs
+++ Augmenting a Textbook/Assets/Scripts/MainMenuButtonManager.cs
@@ -5,7 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuButtonManager : MonoBehaviour
 {
-    public void LoadCameraScene() {
+
+    private GameObject mainMenu;
+    private GameObject helpMenu;
+
+    public void Start()
+    {
+        mainMenu = GameObject.Find("MainMenu");
+        helpMenu = GameObject.Find("HelpMenu");
+
+        helpMenu.SetActive(false);
+    }
+
+    public void LoadCameraScene()
+    {
         SceneManager.LoadScene("Camera");
+    }
+
+    public void ShowHelp()
+    {
+        mainMenu.SetActive(false);
+        helpMenu.SetActive(true);
+    }
+
+    public void HideHelp()
+    {
+        mainMenu.SetActive(true);
+        helpMenu.SetActive(false);
     }
 }
