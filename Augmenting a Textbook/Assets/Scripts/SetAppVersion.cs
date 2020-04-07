f1219f5193b9a2ebf6ca41dbfe25b2917125f101
Thomas <thomasjameswright@hotmail.co.uk>
2020-04-07 23:43:38 +0100

    moved menu to be easier to use

---
+++ Augmenting a Textbook/Assets/Scripts/SetAppVersion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SetAppVersion : MonoBehaviour
+{
+    void Start()
+    {
+        TextMeshProUGUI version = this.GetComponent<TextMeshProUGUI>();
+        version.text = "v " + Application.version;
+    }
+}
