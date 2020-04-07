d90a6a28d0296ecc5f03c301a4eb229674e45013
Thomas <thomasjameswright@hotmail.co.uk>
2020-04-07 22:05:43 +0100

    added value indicators for sliders

--- Augmenting a Textbook/Assets/Scripts/BlinnPhongManager.cs
+++ Augmenting a Textbook/Assets/Scripts/BlinnPhongManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class BlinnPhongManager : MonoBehaviour
 {
@@ -15,6 +16,11 @@ public class BlinnPhongManager : MonoBehaviour
     private Slider shininessSlider;
     private Renderer mainObjectRenderer;
 
+    private TextMeshProUGUI ambientValueText;
+    private TextMeshProUGUI diffuseValueText;
+    private TextMeshProUGUI specularValueText;
+    private TextMeshProUGUI shininessValueText;
+
     private void Update()
     {
         if (GameObject.Find("ARShaderObject") != null && !shaderUI.activeSelf)
@@ -26,6 +32,11 @@ public class BlinnPhongManager : MonoBehaviour
             diffuseSlider = GameObject.Find("DiffuseSlider").GetComponent<Slider>();
             specularSlider = GameObject.Find("SpecularSlider").GetComponent<Slider>();
             shininessSlider = GameObject.Find("ShininessSlider").GetComponent<Slider>();
+
+            ambientValueText = GameObject.Find("AmbientValue").GetComponent<TextMeshProUGUI>();
+            diffuseValueText = GameObject.Find("DiffuseValue").GetComponent<TextMeshProUGUI>();
+            specularValueText = GameObject.Find("SpecularValue").GetComponent<TextMeshProUGUI>();
+            shininessValueText = GameObject.Find("ShininessValue").GetComponent<TextMeshProUGUI>();
         }
         else if (GameObject.Find("ARShaderObject") == null && shaderUI.activeSelf)
         {
@@ -38,6 +49,11 @@ public class BlinnPhongManager : MonoBehaviour
             mainObjectRenderer.material.SetFloat("_DiffuseIntensity", diffuseSlider.value);
             mainObjectRenderer.material.SetFloat("_SpecularIntensity", specularSlider.value);
             mainObjectRenderer.material.SetFloat("_Shininess", shininessSlider.value);
+
+            ambientValueText.text = ambientSlider.value.ToString("F2");
+            diffuseValueText.text = diffuseSlider.value.ToString("F2");
+            specularValueText.text = specularSlider.value.ToString("F2");
+            shininessValueText.text = shininessSlider.value.ToString("F2");
         }
     }
 }
